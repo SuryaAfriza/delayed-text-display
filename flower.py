80df92377e6dc0734bd3fd932947b495adc50503
Surya Afriza <suryaafriza19@gmail.com>
2025-05-01 22:01:21 +0700

    [200~ngubah posisi k sekaligus mengganti jumlah kelopak dan mengubah warna menjadi magenta

--- flower.py
+++ flower.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 
 # Membuat data
 theta = np.linspace(0, 2 * np.pi, 100)  # Membuat 100 titik dari 0 sampai 2pi
-r = np.ones_like(theta)  # Radiusnya tetap 1 (lingkaran)
+k = 5 # Jumlah kelopak bunga
+r = 10 * np.sin(k * theta) # Fungsi sinus untuk membuat kelopak
+
 
 # Membuat plot polar
 plt.subplot(111, projection='polar')  # Mengatur plot ke polar
-plt.plot(theta, r)  # Menggambar lingkaran
-plt.title("Lingkaran Sederhana")  # Judul plot
+plt.plot(theta, r, color='magenta')  # Menggambar bunga dengan warna violet
+plt.title("bunga mawar dan kelopak")  # Judul plot
 plt.show()  # Menampilkan plot
